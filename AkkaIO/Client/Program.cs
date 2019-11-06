e06f2af69aeedf462b11f18b0a39ffbeea43540f
Franz Julius Gruber <franz_gruber@outlook.com>
2019-11-06 17:59:11 +0100

    added akka io folder

---
+++ AkkaIO/Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using Akka.Actor;
+using Akka.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var system = ActorSystem.Create("alcatraz");
+            var manager = system.Tcp();
+
+            try
+            {
+                system.ActorOf(Props.Create(() => new TelnetClient(manager, "127.0.0.1", 9000)), "client");
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                Console.WriteLine(message);
+            }
+            
+        }
+    }
+}
---
+++ AkkaIO/Server/EchoServer.cs
@@ -0,0 +1,38 @@
+﻿using Akka.Actor;
+using Akka.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class EchoServer : UntypedActor
+    {
+
+
+        public EchoServer(int port)
+        {
+            var system = ActorSystem.Create("alcatraz");
+            var manager = system.Tcp();
+            manager.Tell(new Tcp.Bind(Self, new IPEndPoint(IPAddress.Loopback, port)));
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Tcp.Bound)
+            {
+                var bound = message as Tcp.Bound;
+                Console.WriteLine("Listening on {0}", bound.LocalAddress);
+            }
+            else if (message is Tcp.Connected)
+            {
+                var connection = Context.ActorOf(Props.Create(() => new EchoConnection(Sender)));
+                Sender.Tell(new Tcp.Register(connection));
+            }
+            else Unhandled(message);
+        }
+    }
+}
